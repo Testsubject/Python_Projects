c45c30657a517efad9a95bfa5c6fe93f9a5bd9c1
Testsubject <32332240+Testsubject@users.noreply.github.com>
2017-11-10 22:15:21 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,30 @@
+import random
+
+name = input("Enter the hero's name: ")
+print ("Ah, so your name is %s" % (name))
+print ("Please, tell me your stats...")
+
+def hero_stat_input(hero_str, hero_agi, hero_int):
+    print ("Input the strength of the hero")
+    hero_str = int(input("> "))
+    print ("Input the agility of the hero")
+    hero_agi = int(input("> "))
+    print ("Input the intelligence of the hero")
+    hero_int = int(input("> "))
+    return (hero_str, hero_agi, hero_int)
+
+hero_str = 0
+hero_agi = 0
+hero_int = 0
+hero_str, hero_agi, hero_int = hero_stat_input(hero_str, hero_agi, hero_int)
+print ("%s's strength is %s" % (name, hero_str))
+print ("%s's agility is %s" % (name, hero_agi))
+print ("%s's intelligence is %s" % (name, hero_int))
+
+quest = ["slay the dragon", "save the princess"]
+print ("Your quest is to %s" % (random.choice(quest)))
+
+if (quest[1] == True):
+    print ("test")
+else:
+    print ("nope")
