041e2cbf8c32deb11fe06a036250bcf47612cbc8
Testsubject <32332240+Testsubject@users.noreply.github.com>
2017-11-13 07:48:29 -0500

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -17,6 +17,7 @@ class Hero:# All of this can, and probably should, be within a class
                 break
 
     def _set_hero_stats(self):
+        print ("You have a max or 15 points for stats")
         print("Input the strength of the hero")
         self.strength = int(input("> "))
         print("Input the agility of the hero")
@@ -28,19 +29,81 @@ class Hero:# All of this can, and probably should, be within a class
         return sum([self.strength, self.agility, self.intelligence])
 
 def quest_gen(hero):
-    quests = ["slay the dragon", "save the princess", "aquire new magic"] #Changed name to quests since it holds multiple quests
-    quest = random.choice(quests) #Moved this from the print statement so you can remember the selected quest, similar to how you remember the heros name
-    print("Your quest is to %s" % (quest))
-
-    if (quests[0] == quest and hero.strength >= 6): #Checking to see if the selected quest is equal to the dragon quest
-        print("You have slain the dragon!")
-    elif (quests[1] == quest and hero.agility >= 6):
-        print("You haved saved the princess!")
-    elif (quests[2] == quest and hero.intelligence >= 6):
-        print ("You have gained new magic!")
+    print("Your quest can be, slay the dragon, save the princess, or aquire new magic. Which quest do you want?")
+    quest = input("1, 2, or 3: ")
+    if (quest == "1"):
+        print ("Your quest is to slay the dragon.")
+    elif (quest == "2"):
+        print ("Your quest is to save the princess.")
+    elif (quest == "3"):
+        print ("Your quest is to aquire new magic.")
     else:
-        print ("You have failed in your quest.")
+        print ("Please enter 1, 2, or 3.")
+
+def dragon_quest(hero):
+    print ("You have 2 options. You can either go to the town for information, or you can head to the dragon's lair.")
+    dragon_option1 = input("1 or 2: ")
+    if (dragon_option1 == "1"):
+        print ("You decide to go to the town for some questions about the dragon.")
+        print ("You see a man walking. You decide to talk to him about the dragon.")
+        print ("The man said that the dragon comes by to take people to it's lair.")
+        print ("Do you ask for more information?")
+        ask_man1 = input("1 or 2: ")
+        if (ask_man1 == "1"):
+            print ("You ask if there are any weaknesses on the dragon.")
+            print ("The man said that the dragon usually stays away from cold places.")
+            print ("You thank the man for the information and you walk away.")
+        if (ask_man1 == "2"):
+            print ("You thank the man for the information and you walk away, content with your choice.")
+        print ("You notice a magic shop open, do you wish to go to it?")
+        shop1 = input("1 or 2: ")
+        if (shop1 == "1"):
+            print ("You decide to go to the magic shop")
+            print ("The man who owns the shop says he has 3 spells for sale.")
+            print ("He has Fire, Ice, and Electricity.")
+            print ("Do you wish to buy anything? Or leave?")
+            shop2 = input("1 or 2: ")
+            if (shop2 == "1"):
+                print ("You decide to buy something, unfortunatly you only have enough gold to buy one spell.")
+                print ("Fire? Ice? or Electricity?")
+                shop3 = input("1, 2, or 3: ")
+                if (shop3 == "1"):
+                    print ("You decide to buy the Fire spell, you hand the man the money.")
+                if (shop3 == "2"):
+                    print ("You decide to buy the Ice spell, you hand the man the money.")
+                if (shop3 == "3"):
+                    print ("You decide to buy the Electricity spell, you hand the man the money.")
+            if (shop2 == "2"):
+                print ("You decide to head towards the dragon's lair.")
+        if (shop1 == "2"):
+            print ("You decide to head towards the dragon's lair.")
+        print ("As you are approaching the dragon's lair, you feel it begin to get warmer.")
+        print ("You are at the dragon's lair, you are walking very slowely.")
+        print ("It is really hot.")
+        print ("You can see the dragon sleeping, you approach it with your sword drawn.")
+        rock = random.randint(1,2)
+        if (rock == 1):
+            print ("You tripped on a rock and the dragon awoke from its slumber.")
+            print ("The dragon began to attack you.")
+            print ("Do you hide behind a rock? Or attempt to strike it down?")
+            dragon_fight1 = input("1 or 2: ")
+            if (dragon_fight == "1"):
+                print ("You hide behind a rock.")
+        if (rock == 2):
+            print ("You almost tripped on a rock.")
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     hero = Hero()
     quest_gen(hero)
+    dragon_quest(hero)
+
