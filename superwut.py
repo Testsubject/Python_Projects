352df5247da6036008a57ea97b3bae7c5a4751cf
Charles Bayley <cbayley95@gmail.com>
2017-12-06 00:48:16 -0500

    added superwut.py

---
+++ superwut.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""
+What do you think this output will be?
+"""
+
+class A(object):
+    def foo(self):
+        print('A')
+
+class B(A):
+    def foo(self):
+        print('B')
+        super(B, self).foo()
+
+class C(A):
+    def foo(self):
+        print('C')
+        super(C, self).foo()
+
+class D(B,C):
+    def foo(self):
+        print('D')
+        super(D, self).foo()
+
+d = D()
+d.foo()
